c21003a40d06c5f6e2f5a169cfcc2ca196c29ce5
simoganger <simonsoft7@gmail.com>
2021-02-20 09:29:42 +0100

    Initial commit

---
+++ model/src/test/java/com/dreamcashgroup/dcash/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamcashgroup.dcash.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
